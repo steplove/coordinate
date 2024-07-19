259ede2fe95aa285134da2510d93e6657bae71d1
steplove <47933243+steplove@users.noreply.github.com>
2024-07-19 08:13:49 +0700

    update

--- src/pages/ExportForm.js
+++ src/pages/ExportForm.js
@@ -12,7 +12,6 @@ import {
 } from "@mui/material";
 import Swal from "sweetalert2";
 import axios from "axios";
-import { create } from "xmlbuilder2";
 import { BASE_URL } from "../constants/constants";
 
 function ExportForm() {
@@ -21,6 +20,7 @@ function ExportForm() {
   const [status, setStatus] = useState("");
   const [clinic, setClinic] = useState("");
   const [station, setStation] = useState("");
+
   useEffect(() => {
     if (clinic === "41749") {
       setStation("A01");
@@ -40,7 +40,17 @@ function ExportForm() {
       setStation("");
     }
   }, [clinic]);
+
+  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
   const handleExport = async () => {
+    const showProgress = (percentage) => {
+      const progressBar = document.getElementById("progress-bar");
+      const progressText = document.getElementById("progress-text");
+      progressBar.value = percentage;
+      progressText.textContent = `${percentage}%`;
+    };
+
     try {
       const formatDateTime = (date) => {
         const d = new Date(date);
@@ -52,11 +62,15 @@ function ExportForm() {
           .padStart(2, "0")}:${d.getMinutes().toString().padStart(2, "0")}:${d
           .getSeconds()
           .toString()
-          .padStart(2, "0")}`;
+          .padStart(2, "0")}.000Z`;
       };
 
       const formattedStartDate = formatDateTime(startDate);
       const formattedEndDate = formatDateTime(endDate);
+
+      const progressContainer = document.getElementById("progress-container");
+      progressContainer.style.display = "block";
+      showProgress(0);
 
       const response1 = await axios.get(BASE_URL + "/api/BillTransXML", {
         params: {
@@ -64,14 +78,30 @@ function ExportForm() {
           P_ToDate: formattedEndDate,
           P_TFlag: status,
         },
+        onDownloadProgress: (progressEvent) => {
+          const percentCompleted = Math.round(
+            (progressEvent.loaded * 25) / progressEvent.total
+          );
+          showProgress(percentCompleted);
+        },
       });
+
+      await delay(500);
 
       const response2 = await axios.get(BASE_URL + "/api/BillItems", {
         params: {
           P_FromDate: formattedStartDate,
           P_ToDate: formattedEndDate,
         },
+        onDownloadProgress: (progressEvent) => {
+          const percentCompleted = Math.round(
+            25 + (progressEvent.loaded * 25) / progressEvent.total
+          );
+          showProgress(percentCompleted);
+        },
       });
+
+      await delay(500);
 
       const data1 = response1.data;
       const data2 = response2.data;
@@ -80,142 +110,151 @@ function ExportForm() {
       const dateTimeString = new Date(
         now.getTime() + 7 * 60 * 60 * 1000
       ).toISOString();
+      const fileDateString = now.toISOString().slice(0, 10).replace(/-/g, "");
 
-      const xml = create({ version: "1.0", encoding: "windows-874" })
-        .ele("ClaimRec", { System: "OP", PayPlan: "SS", Version: "0.93" })
-        .ele("Header")
-        .ele("HCODE")
-        .txt("12026")
-        .up()
-        .ele("HNAME")
-        .txt("โรงพยาบาล เกษมราษฎร์ศรีบุรินทร์")
-        .up()
-        .ele("DATETIME")
-        .txt(dateTimeString)
-        .up()
-        .ele("SESSNO")
-        .txt("0001")
-        .up()
-        .ele("RECCOUNT")
-        .txt(data1.length + data2.length)
-        .up()
-        .up()
-        .ele("BILLTRAN")
-        .ele("BillItems");
+      let xmlString = `<?xml version="1.0" encoding="windows-874"?>
+<ClaimRec System="OP" PayPlan="SS" Version="0.93">
+<Header>
+<HCODE>12026</HCODE>
+<HNAME>โรงพยาบาล เกษมราษฎร์ศรีบุรินทร์</HNAME>
+<DATETIME>${dateTimeString}</DATETIME>
+<SESSNO>0001</SESSNO>
+<RECCOUNT>${data1.length + data2.length}</RECCOUNT>
+</Header>
+<BILLTRAN>`;
 
       data1.forEach((item) => {
-        xml
-          .txt(
-            `${item.Station}||${item.Authcode}|${item.DTtran}|${item.Hcode}|${item.InvNo}||${item.HN}|${item.MemberNo}|${item.Amount}|${item.Paid}||${item.Tflag}|${item.Pid}|${item.Name}|${item.Hmain}|${item.PayPlan}|${item.ClaimAmt}|${item.OtherPayplan}|${item.OtherPay}`
-          )
-          .up();
+        xmlString += `
+${item.Station}||${item.Authcode}|${item.DTtran}|${item.Hcode}|${item.InvNo}||${item.HN}|${item.MemberNo}|${item.Amount}|${item.Paid}||${item.Tflag}|${item.Pid}|${item.Name}|${item.Hmain}|${item.PayPlan}|${item.ClaimAmt}|${item.OtherPayplan}|${item.OtherPay}`;
       });
 
-      xml.up().ele("BillItems");
+      xmlString += `
+</BILLTRAN>
+<BillItems>`;
 
-      // data2.forEach((item) => {
-      //   xml
-      //     .txt(
-      //       `${item.InvNo}|${item.Sv_date}|${item.BillMuad}|${item.LCCode}|${item.STDCode}||${item.QTY}|${item.UP}|${item.ChargeAmt}|${item.ClaimUP}|${item.CliamAmount}|${item.SvRefID}|${item.ClaimCat}`
-      //     )
-      //     .up();
-      // });
+      data2.forEach((item) => {
+        xmlString += `
+${item.InvNo}|${item.Sv_date}|${item.BillMuad}|${item.LCCode}|${item.STDCode}||${item.QTY}|${item.UP}|${item.ChargeAmt}|${item.ClaimUP}|${item.CliamAmount}|${item.SvRefID}|${item.ClaimCat}`;
+      });
 
-      const xmlString = xml.end({ prettyPrint: true });
+      xmlString += `
+</BillItems>
+</ClaimRec>`;
+
       console.log("Generated XML:", xmlString);
+
       const blob = new Blob([xmlString], { type: "application/xml" });
       const url = URL.createObjectURL(blob);
       const link = document.createElement("a");
       link.href = url;
-      link.setAttribute("download", "exported_data.xml");
+      link.setAttribute("download", `BILLTRAN${fileDateString}.txt`);
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
 
-      Swal.fire("Exported", "Your data has been exported", "success");
+      await delay(500);
+
+      showProgress(100);
+
+      Swal.fire("สำเร็จ", "", "success");
     } catch (error) {
-      console.error("Error exporting data", error);
-      Swal.fire("Error", "There was an error exporting the data", "error");
+      Swal.fire("Error", "ข้อผิดพลาดติดต่อ ICT", "error");
+    } finally {
+      const progressContainer = document.getElementById("progress-container");
+      progressContainer.style.display = "none";
     }
   };
 
   return (
-    <Container maxWidth="sm">
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          mt: 4,
-        }}
-      >
-        <Typography variant="h4" color="primary" gutterBottom>
-          SSO Export KSBR
-        </Typography>
-        <FormControl fullWidth>
-          <InputLabel id="select-clinic-label">เลือกคลินิก</InputLabel>
-          <Select
-            labelId="select-clinic-label"
-            value={clinic}
-            label="เลือกคลินิก"
-            onChange={(e) => setClinic(e.target.value)}
-          >
-            <MenuItem value="41749">กิตรการแพทย์ (41749)</MenuItem>
-            <MenuItem value="41752">แม่ขะจานคลินิก (41752)</MenuItem>
-            <MenuItem value="41751">คลินิกเวชกรรมแพทย์เจนพล (41751)</MenuItem>
-            <MenuItem value="41750">คลินิกหมอไชยวัฒน์ (41750)</MenuItem>
-            <MenuItem value="43851">คลินิกเวชกรรมหมอชัชวาล (43851)</MenuItem>
-            <MenuItem value="43849">คลินิกเวชกรรมหมอนิตยา (43849)</MenuItem>
-            <MenuItem value="43850">
-              คลินิกเวชกรรมแพทย์กนกรัตน์ (43850)
-            </MenuItem>
-          </Select>
-        </FormControl>
-        <TextField
-          fullWidth
-          label="ตั้งแต่วันที่"
-          type="date"
-          variant="standard"
-          InputLabelProps={{ shrink: true }}
-          value={startDate}
-          onChange={(e) => setStartDate(e.target.value)}
-          margin="normal"
-        />
-        <TextField
-          fullWidth
-          label="ถึงวันที่"
-          type="date"
-          variant="standard"
-          InputLabelProps={{ shrink: true }}
-          value={endDate}
-          onChange={(e) => setEndDate(e.target.value)}
-          margin="normal"
-        />
-        <FormControl fullWidth margin="normal">
-          <InputLabel>สถานะการส่ง</InputLabel>
-          <Select value={status} onChange={(e) => setStatus(e.target.value)}>
-            {/* <MenuItem value="">
-              <em>เป็นรายการยอดเบิก</em>
-            </MenuItem> */}
-            <MenuItem value="0">เป็นรายการขอเบิก</MenuItem>
-            <MenuItem value="1">แก้ไขรายการ</MenuItem>
-            <MenuItem value="2">ยกเลิกรายการ</MenuItem>
-          </Select>
-        </FormControl>
+    <div>
+      <Container maxWidth="sm">
         <Box
           sx={{
             display: "flex",
-            justifyContent: "space-between",
-            mt: 3,
-            width: "100%",
+            flexDirection: "column",
+            alignItems: "center",
+            mt: 4,
           }}
         >
-          <Button variant="contained" color="primary" onClick={handleExport}>
-            Export
-          </Button>
+          <Typography variant="h4" color="primary" gutterBottom>
+            SSO Export KSBR
+          </Typography>
+          <FormControl fullWidth>
+            <InputLabel id="select-clinic-label">เลือกคลินิก</InputLabel>
+            <Select
+              labelId="select-clinic-label"
+              value={clinic}
+              label="เลือกคลินิก"
+              onChange={(e) => setClinic(e.target.value)}
+            >
+              <MenuItem value="41749">กิตรการแพทย์ (41749)</MenuItem>
+              <MenuItem value="41752">แม่ขะจานคลินิก (41752)</MenuItem>
+              <MenuItem value="41751">คลินิกเวชกรรมแพทย์เจนพล (41751)</MenuItem>
+              <MenuItem value="41750">คลินิกหมอไชยวัฒน์ (41750)</MenuItem>
+              <MenuItem value="43851">คลินิกเวชกรรมหมอชัชวาล (43851)</MenuItem>
+              <MenuItem value="43849">คลินิกเวชกรรมหมอนิตยา (43849)</MenuItem>
+              <MenuItem value="43850">
+                คลินิกเวชกรรมแพทย์กนกรัตน์ (43850)
+              </MenuItem>
+            </Select>
+          </FormControl>
+          <TextField
+            fullWidth
+            label="ตั้งแต่วันที่"
+            type="date"
+            variant="standard"
+            InputLabelProps={{ shrink: true }}
+            value={startDate}
+            onChange={(e) => setStartDate(e.target.value)}
+            margin="normal"
+          />
+          <TextField
+            fullWidth
+            label="ถึงวันที่"
+            type="date"
+            variant="standard"
+            InputLabelProps={{ shrink: true }}
+            value={endDate}
+            onChange={(e) => setEndDate(e.target.value)}
+            margin="normal"
+          />
+          <FormControl fullWidth margin="normal">
+            <InputLabel>สถานะการส่ง</InputLabel>
+            <Select value={status} onChange={(e) => setStatus(e.target.value)}>
+              <MenuItem value="0">เป็นรายการขอเบิก</MenuItem>
+              <MenuItem value="1">แก้ไขรายการ</MenuItem>
+              <MenuItem value="2">ยกเลิกรายการ</MenuItem>
+            </Select>
+          </FormControl>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              mt: 3,
+              width: "100%",
+            }}
+          >
+            <Button variant="contained" color="primary" onClick={handleExport}>
+              Export
+            </Button>
+          </Box>
+          <Box
+            id="progress-container"
+            sx={{ display: "none", width: "100%", mt: 3 }}
+          >
+            <Typography id="progress-text" variant="body1">
+              0%
+            </Typography>
+            <progress
+              id="progress-bar"
+              value="0"
+              max="100"
+              style={{ width: "100%" }}
+            ></progress>
+          </Box>
         </Box>
-      </Box>
-    </Container>
+      </Container>
+    </div>
   );
 }
 
